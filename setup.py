93fe2dda8073f12ad068b97bbddfbe36c2e50db9
Chiwon Cho <goldworm@icon.foundation>
2020-01-09 15:17:39 +0900

    Update package metadata
    * Add __version__.py
    * Update setup.py
    * Update requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
+import os
 import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+about = {}
+with open(os.path.join(here, "__version__.py"), "r") as f:
+    exec(f.read(), about)
+
 setuptools.setup(
-    name="governor",
-    version="0.0.2",
-    author="goldworm",
-    author_email="goldworm@iconloop.com",
-    description="Governor SCORE handler",
+    name=about["__title__"],
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    description=about["__description__"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/goldworm-icon/governor",
+    url=about["__url__"],
     packages=setuptools.find_packages(),
     classifiers=[
         "License :: OSI Approved :: Apache License",
